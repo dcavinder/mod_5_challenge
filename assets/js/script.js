221f4d2e87261d36f01c9dd87e0a475d70a80d8f
Danielle Cavinder <daniellecavinder@Danielles-MBP.home>
2022-11-23 13:56:16 -0700

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
+//Displays data and time using moment
 var currentDay = moment().format("[Today's date is: ]MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDay)
+
+//Container for the schedule
 var timeSlotContainer = document.querySelector(".container")
+
+//time array
 var timeSlots = [
     {
         hour: '7',
@@ -88,19 +93,22 @@ var timeSlots = [
     }
 ]
 
+//fills in each timeslot
 timeSlots.forEach(schedulePopulate);
 
 function schedulePopulate(item) {
+    //creates div for each time slot to nest inside
     var scheduleRow = document.createElement("div");
     scheduleRow.classList.add("row");
     $(".container").append(scheduleRow); 
 
+    //separates each hour and labels them using information from the timeSlots array
     var hourSlot = document.createElement('p');
     hourSlot.classList.add("hour", "col-2");
     hourSlot.innerText = item.hour + item.period;
     scheduleRow.append(hourSlot);
 
-
+    //Creates a form to add newtasks
     var toDoForm = document.createElement("form");
     var toDoEntry = document.createElement("input");
     toDoEntry.setAttribute("type", "text")
@@ -109,6 +117,7 @@ function schedulePopulate(item) {
     toDoForm.append(toDoEntry);
     scheduleRow.append(toDoForm);
 
+    //classifies time slots into past present and future to color code appropriately
     if (item.time === moment().format("HH")) {
         toDoForm.classList.add("present","col-9");
 
@@ -119,19 +128,22 @@ function schedulePopulate(item) {
         toDoForm.classList.add("past","col-9");
     }
 
+    //save button
     var saveSection = document.createElement('p'); 
     saveSection.classList.add("saveBtn","col-1");
     scheduleRow.append(saveSection);
-
+    //save icon from font awesome
     var save = document.createElement('i'); 
     save.classList.add("fa", "fa-save");
     saveSection.append(save);
-
+    //event listener so that the new task entry is saved when the user clicks the button
     saveSection.addEventListener('click', function(event){
         var newToDo = $(event.target).closest("p").prev().find("input").val()
         item.task = newToDo; 
         localStorage.setItem(item.time, item.task);
     })
+
+    //adds a task list and checks for local storage
     var taskList = document.createElement("p");
     toDoForm.append(taskList);
     taskList.innerText = localStorage.getItem(item.time) ||'';
